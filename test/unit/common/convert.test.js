bfb863a0c885eba0cf7e31e70e4e85ac9f0fe09c
balsoft <balsoft@yandex.ru>
2017-11-26 10:18:05 +0300

    Tests for HTML decoding

--- test/unit/common/convert.test.js
+++ test/unit/common/convert.test.js
@@ -3,10 +3,14 @@ const convert = require('../../../src/common/convert');
 
 const TEST_WINDOWS_1251_ENGLISH = [0x74, 0x65, 0x73, 0x74];
 const TEST_WINDOWS_1251_RUSSIAN = [0xF2, 0xE5, 0xF1, 0xF2];
+const TEST_HTML_SEQUENCE = [0x26, 0x6C, 0x74, 0x3B];
 
 describe('common::convertEncoding', () => {
   it('Should properly convert windows-1251 to utf8', () => {
     assert.equal(convert(Buffer.from(TEST_WINDOWS_1251_ENGLISH, 'binary')), 'test');
     assert.equal(convert(Buffer.from(TEST_WINDOWS_1251_RUSSIAN, 'binary')), 'тест');
   });
+  it('Should decode HTML sequences', ()=>{
+    assert.equal(convert(Buffer.from(TEST_HTML_SEQUENCE, 'binary')), '<');
+  })
 });
